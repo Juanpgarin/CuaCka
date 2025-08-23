f8dba7c5c10868deb000b1eeb4122106bb2e5002
Juan Garin <juanpgarin@gmail.com>
2025-08-23 11:22:21 -0300

    Cuentos Agregados

--- js/main.js
+++ js/main.js
@@ -35,3 +35,61 @@ document.querySelectorAll('.reseña-card').forEach(card => {
     }
   });
 });
+
+// ====== VISOR DE LIBROS ======
+const modal = document.getElementById("visorLibro");
+const imgPagina = document.getElementById("paginaLibro");
+const btnCerrar = document.querySelector(".cerrar");
+const btnPrev = document.querySelector(".prev");
+const btnNext = document.querySelector(".next");
+
+let paginas = ["./img/Cuentos/toto-aprendiendo-a-abrazar"];   // acá cargamos las páginas del libro clickeado
+let indice = 0;     // página actual
+
+// Abrir libro al clickear portada
+document.querySelectorAll(".libro-card").forEach((card, i) => {
+  card.addEventListener("click", () => {
+    // 👇 ejemplo de páginas, podés personalizar por libro
+    if (i === 0) {
+      paginas = [
+        "../img\Cuentos\toto-aprendiendo-a-abrazar/portada",
+        "../../img/Cuentos/libro1-pag2.png",
+        "../../img/Cuentos/libro1-pag3.png"
+      ];
+    } else if (i === 1) {
+      paginas = [
+        "../../img/Cuentos/libro2-pag1.png",
+        "../../img/Cuentos/libro2-pag2.png"
+      ];
+    } else {
+      paginas = [
+        "../../img/Cuentos/libro3-pag1.png"
+      ];
+    }
+
+    indice = 0;
+    mostrarPagina();
+    modal.style.display = "flex";
+  });
+});
+
+// Función para mostrar la página actual
+function mostrarPagina() {
+  imgPagina.src = paginas[indice];
+}
+
+// Botones de navegación
+btnPrev.addEventListener("click", () => {
+  indice = (indice - 1 + paginas.length) % paginas.length;
+  mostrarPagina();
+});
+
+btnNext.addEventListener("click", () => {
+  indice = (indice + 1) % paginas.length;
+  mostrarPagina();
+});
+
+// Cerrar visor
+btnCerrar.addEventListener("click", () => {
+  modal.style.display = "none";
+});
